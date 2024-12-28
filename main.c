0cff5f95e8b99d91db8300b997f121638b24ec70
dhia47 <dachraouidhia47@gmail.com>
2024-12-28 22:04:50 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+
 typedef struct {
     char type[50];
     float value;
@@ -19,6 +22,8 @@ int main() {
     Activity *activities = NULL;
     int count = 0;
     int choice;
+    loadActivities(&activities, &count);
+
 
     do {
         printf("\nCarbon Footprint Calculator\n");
@@ -35,21 +40,27 @@ int main() {
         switch (choice) {
             case 1:
                 addActivity(&activities, &count);
+                loadingAnimation();
                 break;
             case 2:
                 removeActivity(&activities, &count);
+                loadingAnimation();
                 break;
             case 3:
                 viewActivities(activities, count);
+                loadingAnimation();
                 break;
             case 4:
-                //calculateAverageEmissions(activities, count);
+                calculateAverageEmissions(activities, count);
+                loadingAnimation();
                 break;
             case 5:
-                //resetActivities(&activities, &count);
+                resetActivities(&activities, &count);
+                loadingAnimation();
                 break;
             case 6:
-                //displayReport(activities, count);
+                displayReport(activities, count);
+                loadingAnimation();
                 break;
             case 7:
                 printf("Exiting program.\n");
@@ -79,7 +90,7 @@ void addActivity(Activity **activities, int *count) {
 
     (*count)++;
 
-    FILE *file = fopen("C:\\Users\miric\Desktop\activities.txt", "a");
+    FILE *file = fopen("activities.txt", "a");
     if (file != NULL) {
         fprintf(file, "%s %.2f %.2f\n", (*activities)[*count - 1].type, (*activities)[*count - 1].value, (*activities)[*count - 1].emission_factor);
         fclose(file);
@@ -117,7 +128,7 @@ void removeActivity(Activity **activities, int *count) {
 
     (*count)--;
 
-    FILE *file = fopen("C:\\Users\miric\Desktop\activities.txt", "w");
+    FILE *file = fopen("activities.txt", "w");
     if (file != NULL) {
         for (int i = 0; i < *count; i++) {
             fprintf(file, "%s %.2f %.2f\n", (*activities)[i].type, (*activities)[i].value, (*activities)[i].emission_factor);
@@ -142,5 +153,89 @@ void viewActivities(Activity *activities, int count) {
         printf("%d. %s - Value: %.2f, Emission Factor: %.2f\n", i + 1, activities[i].type, activities[i].value, activities[i].emission_factor);
     }
     printf("----------------------------\n");
+}
+
+
+void calculateAverageEmissions(Activity *activities, int count) {
+    if (count == 0) {
+        printf("No activities to calculate emissions for.\n");
+        return;
+    }
+
+    float totalEmissions = 0;
+    for (int i = 0; i < count; i++) {
+        totalEmissions += activities[i].value * activities[i].emission_factor;
+    }
+
+    float averageEmissions = totalEmissions / count;
+    printf("\nAverage Emissions Per Activity: %.2f kg CO2\n", averageEmissions);
+}
+
+void resetActivities(Activity **activities, int *count) {
+    free(*activities);
+    *activities = NULL;
+    *count = 0;
+
+
+    FILE *file = fopen("activities.txt", "w");
+    if (file != NULL) {
+        fclose(file);
+    } else {
+        perror("Failed to clear file");
+    }
+
+    printf("All activities have been reset.\n");
+}
+
+void displayReport(Activity *activities, int count) {
+    if (count == 0) {
+        printf("No activities to display a report for.\n");
+        return;
+    }
+
+    float totalEmissions = 0;
+    printf("\nCarbon Footprint Report:\n");
+    printf("----------------------------\n");
+
+    for (int i = 0; i < count; i++) {
+        float emissions = activities[i].value * activities[i].emission_factor;
+        totalEmissions += emissions;
+        printf("%s: %.2f kg CO2\n", activities[i].type, emissions);
+    }
+
+    printf("----------------------------\n");
+    printf("Total Emissions: %.2f kg CO2\n", totalEmissions);
+}
+void loadingAnimation() {
+    for (int i = 0; i < 3; i++) {
+        printf(".");
+        fflush(stdout);
+        usleep(600000);
+    }
+    printf("\n");
+}
+void loadActivities(Activity **activities, int *count) {
+    FILE *file = fopen("activities.txt", "r");
+    if (file == NULL) {
+        return;
+    }
+
+    char type[50];
+    float value, emission_factor;
+
+    while (fscanf(file, "%s %f %f", type, &value, &emission_factor) == 3) {
+        *activities = realloc(*activities, (*count + 1) * sizeof(Activity));
+        if (*activities == NULL) {
+            perror("Memory allocation failed");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+        strcpy((*activities)[*count].type, type);
+        (*activities)[*count].value = value;
+        (*activities)[*count].emission_factor = emission_factor;
+        (*count)++;
+    }
+
+    fclose(file);
 }
 
